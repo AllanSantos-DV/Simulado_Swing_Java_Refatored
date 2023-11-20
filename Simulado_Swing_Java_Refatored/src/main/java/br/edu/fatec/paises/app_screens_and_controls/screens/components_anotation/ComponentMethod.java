26c83e9783b5767c45cb0a501edcb69ac7a51e57
Allan Santos <115902828+AllanSantos-DV@users.noreply.github.com>
2023-11-19 23:28:32 -0300

    Delete Simulado_Swing_Java_Refatored directory

--- Simulado_Swing_Java_Refatored/src/main/java/br/edu/fatec/paises/app_screens_and_controls/screens/components_anotation/ComponentMethod.java
+++
@@ -1,9 +0,0 @@
-package br.edu.fatec.paises.app_screens_and_controls.screens.components_anotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ComponentMethod {
-
-}
